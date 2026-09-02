9ec7cd09dbf1d038d0957a68ac6679202d5b1515
Stephen <stphnblack@gmail.com>
2026-09-02 16:37:06 -0700

    update test

--- tests/app/markerUtils.test.ts
+++ tests/app/markerUtils.test.ts
@@ -1,6 +1,40 @@
 import { expect, test } from "@playwright/test";
 
-import { radiusGivenZoom } from "../../src/js/map-features/markerUtils";
+import { radiusGivenZoom, determineIsPrimary } from "../../src/js/map-features/markerUtils";
+import { ProcessedPlace } from "../../src/js/model/types";
+
+
+test("determineIsPrimary", () => {
+  const place: ProcessedPlace = {
+    name: "",
+    street: "",
+    city: "",
+    postal_code: "",
+    state: "",
+    country: "",
+    type: "Cafe",
+    encoded: "",
+    consumer_base: 0,
+    coord: [0, 0],
+    url: "",
+  };
+
+  expect(determineIsPrimary({ place })).toEqual(false);
+
+  // Plant-based default nudges are primary.
+  expect(
+    determineIsPrimary({
+      place,
+      default: [{ status: "adopted", date: undefined, org_credit: [] }],
+    }),
+  ).toEqual(true);
+  expect(
+    determineIsPrimary({
+      place,
+      default: [{ status: "pledged", date: undefined, org_credit: [] }],
+    }),
+  ).toEqual(true);
+});
 
 test.describe("radiusGivenZoom", () => {
   test("calculates radius correctly for zoom levels", () => {
